51b8ed494cf5826b189c850f485e9024ff7daf69
MuskanRehni <muskanrehni@gmail.com>
2025-01-02 20:39:24 +0530

    last

--- server/server.js
+++ server/server.js
@@ -9,15 +9,19 @@ dotenv.config(); // Load environment variables from .env file
 
 const app = express();
 
-app.use("*", (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Headers", "*");
+app.use("/", (req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin",process.env.CLIENT_URL);
+    res.setHeader(
+        "Access-Control-Allow-Headers",
+        "Content-type, Authorization"
+    );
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, OPTIONS"
     );
     next();
 });
+
 
 // Middleware
 app.use(express.json()); // Parse incoming JSON data
